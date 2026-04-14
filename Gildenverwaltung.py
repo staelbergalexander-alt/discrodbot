e102e10cebde58557fe04828b74d9c42700f1ab5
staelbergalexander-alt <staelberg.alexander@gmail.com>
2026-04-14 04:56:13 +0200

    Update Gildenverwaltung.py

--- Gildenverwaltung.py
+++ Gildenverwaltung.py
@@ -4,6 +4,7 @@ from discord.ext import commands
 from datetime import datetime
 import os
 import asyncio
+import re
 
 # --- KONFIGURATION (Über Umgebungsvariablen oder Standardwerte) ---
 OFFIZIER_ROLLE_ID = int(os.getenv('OFFIZIER_ROLLE_ID'))
